94d330266871c2f6ca74f59003fe1dee52f30a70
ALCAGENY <rreshiran998@gmail.com>
2024-11-19 22:16:03 -0600

    Cambio que quiere champo Pero se echo a perder el registro

--- src/Componentes/RegistroPaciente/FormularioRegistroPaciente/FormularioRegistroPaciente.jsx
+++ src/Componentes/RegistroPaciente/FormularioRegistroPaciente/FormularioRegistroPaciente.jsx
@@ -46,9 +46,14 @@ export function FormularioRegistroPaciente() {
     if (!formData.apellido_p) newErrors.apellido_p = "El apellido paterno es obligatorio.";
     if (!formData.apellido_m) newErrors.apellido_m = "El apellido materno es obligatorio.";
     if (!formData.genero) newErrors.genero = "El género es obligatorio.";
-    
-    if (!formData.edad) newErrors.edad = "La edad es obligatoria.";
-    else if (isNaN(formData.edad)) newErrors.edad = "La edad debe ser un número válido.";
+
+    if (!formData.edad) {
+      newErrors.edad = "La edad es obligatoria.";
+    } else if (isNaN(formData.edad)) {
+      newErrors.edad = "La edad debe ser un número válido.";
+    } else if (formData.edad < 18 || formData.edad > 100) {
+      newErrors.edad = "La edad debe estar entre 18 y 100 años.";
+    }
 
     if (!formData.peso) newErrors.peso = "El peso es obligatorio.";
     else if (isNaN(formData.peso)) newErrors.peso = "El peso debe ser un número válido.";
@@ -101,126 +106,122 @@ export function FormularioRegistroPaciente() {
 
   return (
     <>
-    <section className="h-full w-full rounded-3xl py-10">
-      <div className="text-center">
-        <Titulos textoTitulo="Registro De Datos Del Paciente" />
-      </div>
-      <div className="flex flex-col md:flex-row gap-8 md:gap-14 p-4 md:p-10 rounded-3xl">
-        <section className="w-full md:w-1/2 space-y-4">
-          <div className="relative">
-            <Parrafos textoParrafo="Apellido Paterno" />
-            <InpuT
-              type="text"
-              name="apellido_p"
-              value={formData.apellido_p}
-              onChange={handleInputChange}
-              className={`border ${errors.apellido_p ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.apellido_p && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.apellido_p}</p>
-            )}
-          </div>
-          <div className="relative">
-            <Parrafos textoParrafo="Edad" />
-            <InpuT
-              type="text"
-              name="edad"
-              value={formData.edad}
-              onChange={handleInputChange}
-              className={`border ${errors.edad ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.edad && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.edad}</p>
-            )}
-          </div>
-          <div className="relative">
-            <Parrafos textoParrafo="Altura CM" />
-            <InpuT
-              type="text"
-              name="altura"
-              value={formData.altura}
-              onChange={handleInputChange}
-              className={`border ${errors.altura ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.altura && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.altura}</p>
-            )}
-          </div>
-          <div className="relative">
-            <Parrafos textoParrafo="Estado" />
-            <InpuT
-              type="text"
-              name="estado"
-              value={formData.estado}
-              onChange={handleInputChange}
-              className={`border ${errors.estado ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.estado && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.estado}</p>
-            )}
-          </div>
-        </section> {/* SEGUNDA SECCIION */}
-        <section className="w-full md:w-1/2 space-y-4">
-        <div className="relative">
-            <Parrafos textoParrafo="Apellido Materno" />
-            <InpuT
-              type="text"
-              name="apellido_m"
-              value={formData.apellido_m}
-              onChange={handleInputChange}
-              className={`border ${errors.apellido_m ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.apellido_m && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.apellido_m}</p>
-            )}
-          </div>
-          <div className="relative">
-            <Parrafos textoParrafo="Genero" />
-            <InpuT
-              type="text"
-              name="genero"
-              value={formData.genero}
-              onChange={handleInputChange}
-              className={`border ${errors.genero ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.genero && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.genero}</p>
-            )}
-          </div>
-          <div className="relative">
-            <Parrafos textoParrafo="Peso KG" />
-            <InpuT
-              type="text"
-              name="peso"
-              value={formData.peso}
-              onChange={handleInputChange}
-              className={`border ${errors.peso ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.peso && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.peso}</p>
-            )}
-          </div>
-          <div className="relative">
-            <Parrafos textoParrafo="Municipio" />
-            <InpuT
-              type="text"
-              name="municipio"
-              value={formData.municipio}
-              onChange={handleInputChange}
-              className={`border ${errors.municipio ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
-            />
-            {errors.municipio && (
-              <p className="text-red-500 text-sm absolute bottom-2 left-2 transition-all duration-500 ease-in-out opacity-100">{errors.municipio}</p>
-            )}
-          </div>
-        </section>
-      </div>
-      <div className="mt-3 text-center">
-        <Boton textoBoton="Siguiente" onClick={handleGuardarClick} />
-      </div>
-    </section>
-    {isModalOpen && <ModalDatosFamiliar onClose={handleCloseModal} />}
-  </>
-  
+      <section className="h-full w-full rounded-3xl py-10">
+        <div className="text-center">
+          <Titulos textoTitulo="Registro De Datos Del Paciente" />
+        </div>
+        <div className="flex flex-col md:flex-row gap-8 md:gap-14 p-4 md:p-10 rounded-3xl">
+          <section className="w-full md:w-1/2 space-y-4">
+            <div className="relative">
+              <Parrafos textoParrafo="Apellido Paterno" />
+              <InpuT
+                type="text"
+                name="apellido_p"
+                value={formData.apellido_p}
+                onChange={handleInputChange}
+                className={`border ${errors.apellido_p ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
+              />
+              {errors.apellido_p && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.apellido_p}</p>}
+            </div>
+
+            <div className="relative">
+              <Parrafos textoParrafo="Edad" />
+              <InpuT
+                type="text"
+                name="edad"
+                value={formData.edad}
+                onChange={handleInputChange}
+                className={`border ${errors.edad ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
+              />
+              {errors.edad && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.edad}</p>}
+            </div>
+
+            <div className="relative">
+              <Parrafos textoParrafo="Altura CM" />
+              <InpuT
+                type="text"
+                name="altura"
+                value={formData.altura}
+                onChange={handleInputChange}
+                className={`border ${errors.altura ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
+              />
+              {errors.altura && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.altura}</p>}
+            </div>
+
+            <div className="relative">
+              <Parrafos textoParrafo="Estado" />
+              <div className="bg-ColorFondo bg-opacity-70 w-full sm:p-1 md:p-2 rounded-full mt-2 text-sm sm:text-base md:text-lg">
+                Chiapas
+              </div>
+            </div>
+          </section>
+
+          <section className="w-full md:w-1/2 space-y-4">
+            <div className="relative">
+              <Parrafos textoParrafo="Apellido Materno" />
+              <InpuT
+                type="text"
+                name="apellido_m"
+                value={formData.apellido_m}
+                onChange={handleInputChange}
+                className={`border ${errors.apellido_m ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
+              />
+              {errors.apellido_m && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.apellido_m}</p>}
+            </div>
+
+            <div className="relative">
+              <Parrafos textoParrafo="Género" />
+              <select
+                name="genero"
+                value={formData.genero}
+                onChange={handleInputChange}
+                className={`border ${errors.genero ? 'border-red-500' : 'border-none'} bg-ColorFondo bg-opacity-70 w-full sm:p-1 md:p-2 rounded-full mt-2 text-sm sm:text-base md:text-lg transform transition-transform duration-500 hover:scale-105`}
+              >
+                <option value="">Seleccione</option>
+                <option value="Hombre">Hombre</option>
+                <option value="Mujer">Mujer</option>
+                <option value="Prefiero no decirlo">Prefiero no decirlo</option>
+              </select>
+              {errors.genero && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.genero}</p>}
+            </div>
+
+            <div className="relative">
+              <Parrafos textoParrafo="Peso KG" />
+              <InpuT
+                type="text"
+                name="peso"
+                value={formData.peso}
+                onChange={handleInputChange}
+                className={`border ${errors.peso ? 'border-red-500' : 'border-none'} p-2 rounded-full`}
+              />
+              {errors.peso && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.peso}</p>}
+            </div>
+
+            <div className="relative">
+              <Parrafos textoParrafo="Municipio" />
+              <select
+                name="municipio"
+                value={formData.municipio}
+                onChange={handleInputChange}
+                className={`border ${errors.municipio ? 'border-red-500' : 'border-none'} bg-ColorFondo bg-opacity-70 w-full sm:p-1 md:p-2 rounded-full mt-2 text-sm sm:text-base md:text-lg transform transition-transform duration-500 hover:scale-105`}
+              >
+                <option value="">Seleccione</option>
+                <option value="Suchiapa">Suchiapa</option>
+                <option value="Tuxtla">Tuxtla</option>
+                <option value="San Cristóbal">San Cristóbal</option>
+                <option value="Tapachula">Tapachula</option>
+              </select>
+              {errors.municipio && <p className="text-red-500 text-sm absolute bottom-2 left-2">{errors.municipio}</p>}
+            </div>
+          </section>
+        </div>
+
+        <div className="text-center mt-4">
+          <Boton onClick={handleGuardarClick} textoBoton="Guardar" />
+        </div>
+
+        {isModalOpen && <ModalDatosFamiliar onClose={handleCloseModal} />}
+      </section>
+    </>
   );
 }
